45af11cf397efb2d22aa259c65e036cb25c87624
fakhrulhasan2002 <fakhrulhasan2002@gmail.com>
2022-04-03 14:45:07 -0400

    Second commit

--- src/main/java/config/TestData.java
+++ src/main/java/config/TestData.java
@@ -7,4 +7,5 @@ public final class TestData extends TestBase {
 	public static final String username= prop.getProperty("username");
 	public static final String password= prop.getProperty("password");
 
+	public static final String password1= prop.getProperty("password1");
 }
